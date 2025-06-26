2ecde45b7216cad285a0b996934cd8c1e5fb400b
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2025-06-26 15:28:12 +0700

    Patching up version after merging files

--- frontend/src/shared/services/categories.ts
+++ frontend/src/shared/services/categories.ts
@@ -1,20 +1,21 @@
 import {axiosInstance} from "@/shared/services/axios/axios.ts";
+import type {Category} from "@/shared/types/type.ts";
 
 export class Categories{
-  public static async getCategories(limit : number = 10){
+  public static async getCategories(limit : number = 10) : Promise<Category[]>{
     const response = await axiosInstance.get(`categories?limit=${limit}`);
 
     if (response.status === 200)
-      return response.data;
+      return response.data as Category[];
     else return [];
   }
 
-  public static async getCategory(id : number){
+  public static async getCategory(id : number): Promise<Category>{
     const response = await axiosInstance.get(`categories/${id}`);
 
     if (response.status === 200)
-      return response.data;
-    else return {};
+      return response.data as Category;
+    else return {} as Category;
   }
 
 }
